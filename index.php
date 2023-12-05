fcc85843f4b3d1534113e869bbdf21494ce3059d
MatteoTuveri <matteo.tuveri16@gmail.com>
2023-12-05 19:16:23 +0100

    Add genres

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <?php
-include __DIR__ .'/Models/Movie.php';
-include __DIR__.'/Functions/functions.php';
 include __DIR__.'/server.php';
+include __DIR__ .'/Models/Movie.php';
 
 ?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/db5152b6bb.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>PHP Movies</title>
@@ -22,19 +19,9 @@ include __DIR__.'/server.php';
     <header></header>
     <main class="container">
         <div class="row">
-            <?php foreach ($movies as $item) {?>
-                
-                <div class="col-3">
-                    <div class="card" style="width: 18rem;">
-                        <img src="<?= $item->poster_path ?>" class="card-img-top" alt="<?= $item->title?>">
-                        <div class="card-body">
-                            <h3 class="card-title"><?= $item->title ?></h3>
-                            <p class="card-text"><?= $item->overview ?></p>
-                            <p class="card-text"><?= $item->vote_avarege ?></p>
-                        </div>
-                    </div>
-                </div>
-            <?php } ?>
+            <?php foreach ($movies as $item) {
+                $item->getCard();
+            }?>
         </div>
 
     </main>
--- server.php
+++ server.php
@@ -3,5 +3,4 @@ $filemovie = file_get_contents(__DIR__.'/DB/movie_db.json');
 $list = json_decode($filemovie, true);
 $filegenre = file_get_contents(__DIR__.'/DB/genre_db.json');
 $genres = json_decode($filegenre, true);
-$movies = movieList($list);
 ?>
